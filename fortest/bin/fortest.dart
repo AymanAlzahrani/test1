92a4b305dbb4ac9055721462e9909ea07b4d224f
ayman <aymangormallah@gmail.com>
2025-04-21 12:19:32 +0300

    first commit

---
+++ fortest/bin/fortest.dart
@@ -0,0 +1,5 @@
+import 'package:fortest/fortest.dart' as fortest;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fortest.calculate()}!');
+}
---
+++ fortest/test/fortest_test.dart
@@ -0,0 +1,8 @@
+import 'package:fortest/fortest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
